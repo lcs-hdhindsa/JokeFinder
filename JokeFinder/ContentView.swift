d539e8e706c9dd3783d588aebb459b3a52382e2a
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-04-03 09:09:39 -0400

    Initial Commit

---
+++ JokeFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeFinder
+//
+//  Created by Harshan Dhindsa on 2025-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Harshan Dhindsa on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
